c67397f5433ab71c50f620990daba0fd1bc139f3
Nicole-Malaquias <nicolemalaquias91@gmail.com>
2021-04-05 14:02:10 -0300

    adicionando estrutura basica do js

--- script.js
+++ script.js
@@ -62,3 +62,66 @@ let amountMovie = 0
 
 
 
+
+let collum1 = document.getElementById("line1")
+collum1.addEventListener('click', function(){
+    let col = 'coluna_1'
+    jogada()
+    movi(col)
+})
+
+let collum2 = document.getElementById("line2")
+collum2.addEventListener('click', function(){
+    jogada()
+
+})
+
+let collum3 = document.getElementById("line3")
+collum2.addEventListener('click', function(){
+    jogada()
+
+})
+
+let collum4 = document.getElementById("line4")
+collum2.addEventListener('click', function(){
+    jogada()
+
+})
+
+let collum5 = document.getElementById("line5")
+collum2.addEventListener('click', function(){
+    jogada()
+
+})
+let collum6 = document.getElementById("line6")
+collum2.addEventListener('click', function(){
+    jogada()
+
+})
+let collum7 = document.getElementById("line7")
+collum2.addEventListener('click', function(){
+    jogada()
+
+})
+
+function movi(col,movi_player) {
+    isWinner()  
+}
+
+function isWinner() {
+
+}
+
+function reset() {
+
+}
+
+
+function jogada() {
+   
+}
+
+function movie_player() {
+    
+
+}
