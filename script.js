976e3a13e8cce60c0dc096431ceb8b7d69f78b71
Matheus Paiva <matheeus_paiva@hotmail.com>
2021-04-05 13:43:35 -0300

    variaveis globais

---
+++ script.js
@@ -0,0 +1,64 @@
+let tabuleiro = ['0,0,0,0,0,0,0',
+                 '0,0,0,0,0,0,0',
+                 '0,0,0,0,0,0,0',
+                 '0,0,0,0,0,0,0',
+                 '0,0,0,0,0,0,0',
+                 '0,0,0,0,0,0,0',
+            ]
+let amountPlayer1 = 0
+let amountPlayer2 = 0
+let amountMovie = 0
+
+
+//function start
+
+
+//function start
+
+
+
+
+//function move
+
+
+
+//function move
+
+
+
+
+
+//verifica vitória
+
+
+//verifica vitória
+
+
+
+
+
+//verifica empate
+
+
+//verifica empate
+
+
+
+
+//reset
+
+
+
+//reset
+
+
+
+
+//play again
+
+
+
+//play again
+
+
+
