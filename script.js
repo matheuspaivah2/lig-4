62f29f4c800d3612ae3bbf9db694b6ede06f65da
Matheus Paiva <matheeus_paiva@hotmail.com>
2021-04-08 16:49:19 -0300

    botao end da tela empate funcionando

--- script.js
+++ script.js
@@ -816,3 +816,18 @@ btReset.addEventListener('click', ()=>{
 })
 
 //button reset
+
+
+//bt end tela empate
+
+const btEndEmpate = document.getElementById('bt-end-draw')
+btEndEmpate.addEventListener('click', ()=>{
+  const telaEmpate = document.getElementById('draw')
+  telaEmpate.classList.add('hidden')
+
+  resultadoFinal(point)
+
+})
+
+
+//bt end tela empate
